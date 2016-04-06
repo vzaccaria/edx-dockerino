af0181515ececf920cdb0a3f0643abda70a97aa4
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2016-04-06 14:14:25 +0200

    minor: some minor work

--- index.js
+++ index.js
@@ -1,37 +1,59 @@
 #!/usr/bin/env node
 'use strict';
 
-/* eslint quotes: [0], strict: [0] */
+require('babel-polyfill');
 
-var _require =
-// $r.stdin() -> Promise  ;; to read from stdin
-require('zaccaria-cli');
+var app = require('koa')();
+var router = require('koa-router')();
 
-var $d = _require.$d;
-var $o = _require.$o;
-var $f = _require.$f;
+app.use(router.routes()).use(router.allowedMethods());
 
+app.use(router.routes());
 
-var getOptions = function getOptions(doc) {
-    "use strict";
+router.get("/", regeneratorRuntime.mark(function _callee() {
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+        while (1) {
+            switch (_context.prev = _context.next) {
+                case 0:
+                    this.response.body = [1, 2, 3, 4, 5];
+                    this.response.status = 200;
 
-    var o = $d(doc);
-    var help = $o('-h', '--help', false, o);
-    return {
-        help: help
-    };
-};
-
-var main = function main() {
-    $f.readLocal('docs/usage.md').then(function (it) {
-        var _getOptions = getOptions(it);
-
-        var help = _getOptions.help;
-
-        if (help) {
-            console.log(it);
+                case 2:
+                case 'end':
+                    return _context.stop();
+            }
         }
-    });
-};
+    }, _callee, this);
+}));
 
-main();
+app.listen(3000, function () {
+    console.log("App listening on port 3000.");
+});
+
+/* eslint quotes: [0], strict: [0] */
+// var {
+//     $d, $o, $f
+//     // $r.stdin() -> Promise  ;; to read from stdin
+// } = require('zaccaria-cli')
+//
+// var getOptions = doc => {
+//     "use strict"
+//     var o = $d(doc)
+//     var help = $o('-h', '--help', false, o)
+//     return {
+//         help
+//     }
+// }
+//
+// var main = () => {
+//     $f.readLocal('docs/usage.md').then(it => {
+//         var {
+//             help
+//         } = getOptions(it);
+//         if (help) {
+//             console.log(it)
+//         }
+//     })
+// }
+//
+// main()
--- src/index.js
+++ src/index.js
@@ -1,27 +1,47 @@
+import "babel-polyfill";
+
+var app = require('koa')();
+var router = require('koa-router')();
+
+app .use(router.routes())
+    .use(router.allowedMethods());
+
+app.use(router.routes());
+
+router.get("/", function*() {
+    this.response.body = [1, 2, 3, 4, 5];
+    this.response.status = 200
+});
+
+app.listen(3000, () => {
+    console.log("App listening on port 3000.");
+});
+
+
 /* eslint quotes: [0], strict: [0] */
-var {
-    $d, $o, $f
-    // $r.stdin() -> Promise  ;; to read from stdin
-} = require('zaccaria-cli')
-
-var getOptions = doc => {
-    "use strict"
-    var o = $d(doc)
-    var help = $o('-h', '--help', false, o)
-    return {
-        help
-    }
-}
-
-var main = () => {
-    $f.readLocal('docs/usage.md').then(it => {
-        var {
-            help
-        } = getOptions(it);
-        if (help) {
-            console.log(it)
-        }
-    })
-}
-
-main()
+// var {
+//     $d, $o, $f
+//     // $r.stdin() -> Promise  ;; to read from stdin
+// } = require('zaccaria-cli')
+//
+// var getOptions = doc => {
+//     "use strict"
+//     var o = $d(doc)
+//     var help = $o('-h', '--help', false, o)
+//     return {
+//         help
+//     }
+// }
+//
+// var main = () => {
+//     $f.readLocal('docs/usage.md').then(it => {
+//         var {
+//             help
+//         } = getOptions(it);
+//         if (help) {
+//             console.log(it)
+//         }
+//     })
+// }
+//
+// main()
