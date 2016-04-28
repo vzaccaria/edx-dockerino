7bba227b6a644e3cb60f9091506ef81d183dadb5
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2016-04-28 14:20:32 +0200

    feat: add makefile and tls verify to kube-up

--- docker/k8s-docker-compose/kube-up.sh
+++ docker/k8s-docker-compose/kube-up.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+export DOCKER_TLS_VERIFY=true
+
 require_command_exists() {
     command -v "$1" >/dev/null 2>&1 || { echo "$1 is required but is not installed. Aborting." >&2; exit 1; }
 }
